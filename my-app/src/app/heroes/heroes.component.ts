d43e66622cbf1a07544ff87ca5727404bc0f5a6b
GoobyeEarth <ra4chrome.ctrl@gmail>
2018-03-18 00:06:29 +0900

    refs master

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
