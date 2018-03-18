89c55ac4fe39d1a5e37b3a1bbb63559e96013852
GoobyeEarth <ra4chrome.ctrl@gmail>
2018-03-18 09:58:50 +0900

    5

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,7 +5,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
+import {HeroService} from "./hero.service"; // <-- NgModel lives here
 
 
 
@@ -13,13 +16,14 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
